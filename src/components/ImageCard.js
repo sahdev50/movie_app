d53214df589433dbc82ab21f58cd8d75247570db
sahdev50 <63721612+sahdev50@users.noreply.github.com>
2020-12-29 15:38:43 +0530

    Update ImageCard.js

--- src/components/ImageCard.js
+++ src/components/ImageCard.js
@@ -7,9 +7,9 @@ const ImageCard = ({image, name}) => {
     return (
         
         
-        <div className='card2 col-lg-6 col-md-6'>
+        <div className='card2 col-lg-4 col-md-6'>
             <Tilt className="Tilt" options={{ max: 18, speed: 200, scale:1 }}>
-                    <div className='content box Tilt-inner col-md-6'>
+                    <div className='content box Tilt-inner col-sm-6'>
                         <h4>{name}</h4>
                         <img className='card-img singleImage' src={image} alt='related pic'/>
                     </div>
